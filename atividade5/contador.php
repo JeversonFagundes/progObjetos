e8321e898e4d0fc0a17def8632e1b451fba8e2c2
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2024-09-26 11:28:23 -0300

    atividade6 resolvida

---
+++ atividade5/contador.php
@@ -0,0 +1,28 @@
+<?php
+
+class Contador
+{
+
+    private $valorContador;
+
+    public function setarValorNoContador($v)
+    {
+        $this->valorContador = $v;
+    }
+
+    public function zerar($v0)
+    {
+
+
+    }
+    public function incrementar($vi) {
+
+       
+    }
+
+    public function getContador()
+    {
+
+        return $this->valorContador;
+    }
+}
--- atividade5/index.php
+++ atividade5/index.php
@@ -1,31 +1,13 @@
 <?php
 
-class Contador{
-
-    private $valorContador;
-
-    public function zerar() {
-
-    }
-    public function incrementar() {
-
-    }
-    public function setarValorNoContador($v) {
-
-        $this->valorContador = $v;
-    }
-    public function getContador() {
-
-        return $this->valorContador;
-    }
-}
+require_once "contador.php";
 
 $contador1 = new Contador;
 
-$contador1->setarValorNoContador(10);
+$contador1->setarValorNoContador(20);
 
-echo "O valor do contador é:" . " " . $contador1->getContador();
-
+echo "O valor do contador é:" . " " . $contador1->getContador() . "<br><br>";
 
 ?>
+
 
